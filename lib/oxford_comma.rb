0d5d033b693cd9087c25ba4673757ffe3f8b0a39
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-17 21:06:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,3 @@ def oxford_comma(array)
  array[-1] = "and " + array[-1]
  array.join(', ')
 end
-
-#def oxford_comma(list)
- # return list.join(' and ') if list.size < 3
-  #list[-1] = "and " + list[-1]
-  #list.join(', ')
-#end
